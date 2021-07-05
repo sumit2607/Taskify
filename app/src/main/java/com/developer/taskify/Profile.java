5dd44deea6463652a1c6ec0571b3b68b750e243d
mansi0321 <angelmansi1996@gmail.com>
2021-07-05 21:22:29 +0530

    Initial commit

---
+++ app/src/main/java/com/developer/taskify/Profile.java
@@ -0,0 +1,25 @@
+package com.developer.taskify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Profile extends AppCompatActivity {
+     private TextView mbtnsignout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mbtnsignout = findViewById(R.id.mbtnsignout);
+        mbtnsignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, CreateNewTask.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
